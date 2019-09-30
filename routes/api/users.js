513c7550238d027c51e61009f37a497c93209d5d
unknown <dantecypress@gmail.com>
2019-09-29 23:20:54 -0700

    Req and Body Validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
 
 //@route GET api/users
-//@desc  Test route
+//@desc  Reg user
 //@access Public
-router.get("/", (req, res) => res.send("User Route"));
+router.post(
+  "/",
+  [
+    check("name", "Required")
+      .not()
+      .isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Please enter a password with 6 or more").isLength({
+      min: 6
+    })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    console.log(req.body);
+
+    res.send("User Route");
+  }
+);
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 //Connect DB
 connectDB();
 
+//init Middleware
+app.use(express.json({ extended: false }));
 app.get("/", (req, res) => res.send("API Running"));
 
 //Define Routes
