08ab501077678c8dc7716cb396d4ae673605d79a
unknown <dantecypress@gmail.com>
2019-10-06 22:01:29 -0700

    Login route completed

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,8 +2,13 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const User = require("../../models/User");
-//@route GET api/auth
-//@desc  Test route
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+//@route Post api/auth
+//@desc  Auth user & get token
 //@access Public
 router.get("/", auth, async (req, res) => {
   try {
@@ -14,4 +19,48 @@ router.get("/", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    console.log(req.body);
+
+    const { email, password } = req.body;
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ errors: [{ msg: "Invalid" }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ errors: [{ msg: "Invalid" }] });
+      }
+
+      const payload = {
+        user: { id: user.id }
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 module.exports = router;
